ced2fe2a7cc407f142e0db193eb78d5d7488bfc0
Jasper De Jonghe <jasper.dejonghe@leerling.go-ao.be>
2024-01-09 20:24:06 +0100

    added new blade

--- scenes/Character.gd
+++ scenes/Character.gd
@@ -35,7 +35,7 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, 20)
+		velocity.x = move_toward(velocity.x, 0, 25)
 
 	move_and_slide()
 
