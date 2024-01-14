48d03c097c578947c3ee29f3ddfc8552b37c9edc
Jasper De Jonghe <Jasper.De.Jonghe.07@gmail.com>
2024-01-14 20:13:18 +0100

    finished level 5 and fixed the enemy

--- scenes/jumpBoost.gd
+++ scenes/jumpBoost.gd
@@ -2,8 +2,10 @@ extends Area2D
 
 @onready var game_manager = %GameManager
 @onready var player = %CharacterBody2D
+@onready var sprite_2d = $Sprite2D
 
 func _on_body_entered(body):
 	if (body.name == "CharacterBody2D"):
-		queue_free()
+		sprite_2d.animation = "pick"
 		player.JUMP_VELOCITY = -1500.0
+		queue_free()
