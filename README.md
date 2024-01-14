19f721b676eb0ed334241bf6e0e0fb45450aa2f1
Jasper De Jonghe <Jasper.De.Jonghe.07@gmail.com>
2024-01-14 20:15:15 +0100

    cleared everything for backup

--- README.md
+++
@@ -1,2 +0,0 @@
-# Game Godot
- My first game during 30NitesOfCode
--- scenes/levels/levelSelector2.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-func _on_previous_pressed():
-	get_tree().change_scene_to_file("res://scenes/levels/levelSelector1.tscn")
-
-func _on_level_5_pressed():
-	get_tree().change_scene_to_file("res://scenes/levels/level5.tscn")
